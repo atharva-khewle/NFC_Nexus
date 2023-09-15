8107198552269dbb60b77a9a9804805c3669f513
Atharva <atharvamainwork@gmail.com>
2023-09-15 17:27:17 +0530

    nexus project

---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export const Homepage = () => {
+  return (
+    <div className='jjj '><div className="banner " background="cover" style={{height:"100vh",width:"50vw"}} >
+    <img src="https://cdn.blablacar.com/kairos/assets/images/carpool_only_small-09eb14eafab9352951e1.svg" alt=""/>
+  </div></div>
+  )
+}
